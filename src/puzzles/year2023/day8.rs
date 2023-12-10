4e993e5c6be63a727ccec681c6a10ff2e6c4786e
Shirotha <zimmermann-sven@online.de>
2023-12-10 18:12:47 +0100

    2023-8 input2 failing

--- src/puzzles/year2023/day8.rs
+++ src/puzzles/year2023/day8.rs
@@ -2,6 +2,7 @@ use std::{
     borrow::Cow,
     collections::HashSet
 };
+use itertools::Itertools;
 use nom::{
     IResult,
     bytes::complete::tag,
@@ -9,6 +10,7 @@ use nom::{
     sequence::{separated_pair, delimited, preceded},
     combinator::iterator, multi::many1
 };
+use num::Integer;
 use tap::{Tap, Pipe};
 
 use crate::{*, parse::*};
@@ -75,7 +77,6 @@ impl<'a> BinaryGraph<'a> {
         self.edges[node] = (left, right);
         node
     }
-
     #[inline]
     fn moveto(&mut self, instruction: &char) -> bool {
         match instruction {
@@ -85,25 +86,61 @@ impl<'a> BinaryGraph<'a> {
         }
         self.pos.iter().all( |p| self.goal.contains(p) )
     }
+    fn cycles(&self, instructions: &[char]) -> Vec<(usize, usize)> {
+        // ASSERT: no goal is met before the cycle starts
+        // ASSERT: the cycle only contains a single goal once
+        let len = instructions.len();
+        self.pos.iter().map( |start| {
+            let (mut pos, mut i, mut g) = (*start, 0, None);
+            loop {
+                pos = match instructions[i % len] {
+                    'L' => self.edges[pos].0,
+                    'R' => self.edges[pos].1,
+                    _ => panic!()
+                };
+                i += 1;
+                if self.goal.contains(&pos) {
+                    if let Some(g) = g {
+                        return (g, i - g);
+                    } else {
+                        g = Some(i);
+                    }
+                }
+            }
+        } ).collect_vec()
+    }
 }
 
-fn instructions(input: &str) -> IResult<&str, impl Iterator<Item = char>> {
-    let (input, instructions) = many1(one_of("LR"))(input)?;
-    Ok((input, instructions.into_iter().cycle()))
+#[inline]
+fn instructions(input: &str) -> IResult<&str, Vec<char>> {
+    many1(one_of("LR"))(input)
+}
+
+fn sync(a: (usize, usize), b: (usize, usize)) -> (usize, usize) {
+    let (a, m, b, n) = (a.0 as i128, a.1 as i128, b.0 as i128, b.1 as i128);
+    let egcd = m.extended_gcd(&n);
+    let (q, r) = (b - a).div_mod_floor(&egcd.gcd);
+    if r != 0 { panic!(); }
+    let p = m / egcd.gcd * n;
+    let min = a.min(b);
+    let o = (q * m * egcd.x + min - a).mod_floor(&p) + min;
+    (o as usize, p as usize)
 }
 
 pub fn part1(input: &str) -> Answer {
     parse(input, separated_pair(instructions, line_ending, BinaryGraph::parse_sequential))?
         .pipe( |(inst, mut graph)| 
-            inst.take_while( |inst| !graph.moveto(inst) ).count() + 1
+            inst.into_iter().cycle()
+                .take_while( |inst| !graph.moveto(inst) ).count() + 1
         )
         .pipe( |result| Ok(Cow::Owned(result.to_string())) )
 }
-
+// FIXME: 11623902991577 is too low
 pub fn part2(input: &str) -> Answer {
     parse(input, separated_pair(instructions, line_ending, BinaryGraph::parse_parallel))?
-        .pipe( |(inst, mut graph)| 
-            inst.take_while( |inst| !graph.moveto(inst) ).take(1_000_000).count() + 1
+        .pipe( |(inst, graph)| 
+            graph.cycles(&inst).into_iter()
+                .fold((1, 1), sync).0
         )
         .pipe( |result| Ok(Cow::Owned(result.to_string())) )
 }
