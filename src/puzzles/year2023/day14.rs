c6d60ee2666c2d29e23c3c69d2edef40543e5fa7
Shirotha <zimmermann-sven@online.de>
2023-12-20 14:17:19 +0100

    test2 failing

--- src/puzzles/year2023/day14.rs
+++ src/puzzles/year2023/day14.rs
@@ -1,5 +1,5 @@
 use std::borrow::Cow;
-use ndarray::{Array2, ShapeBuilder, Axis};
+use ndarray::prelude::*;
 use nom::IResult;
 use tap::Pipe;
 
@@ -21,6 +21,14 @@ impl Tile {
             _ => panic!()
         }
     }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Dir {
+    N,
+    W,
+    S,
+    E
 }
 
 struct Platform(Array2<Tile>);
@@ -57,23 +65,80 @@ impl Platform {
         let data = Array2::from_shape_vec((width, height).f(), buffer).unwrap();
         Ok(("", Self(data)))
     }
-    fn tilt_north(&mut self) {
-        let (w, h) = self.0.dim();
-        for y in 1..h {
-            for x in 0..w {
-                if self.0[[x, y]] != Tile::Round { continue; }
-                let mut i = y;
-                while i != 0 && self.0[[x, i - 1]] == Tile::Empty {
-                    i -= 1;
-                }
-                if i == y { continue; }
-                self.0[[x, i]] = Tile::Round;
-                self.0[[x, y]] = Tile::Empty;
+    fn tilt(&mut self, dir: Dir) {
+        #[inline(always)]
+        fn increment(x: &mut usize, reverse: bool) {
+            if reverse {
+                *x -= 1
+            } else {
+                *x += 1
             }
+        }
+        #[inline]
+        fn tilt_view(mut view: ArrayViewMut2<Tile>, reverse: bool) {
+            let (inner_len, outer_len) = view.dim();
+            /*/ DEBUG
+            for i in 0..outer_len {
+                for j in 0..inner_len {
+                    match view[[j, i]] {
+                        Tile::Empty => print!("."),
+                        Tile::Round => print!("O"),
+                        Tile::Cube => print!("#")
+                    }
+                }
+                println!();
+            }
+            println!();
+            // DEBUG end */
+            let (min, max) = if reverse {
+                (outer_len - 1, 0)
+            } else {
+                (0, outer_len - 1)
+            };
+            let mut outer = min;
+            while outer != max {
+                increment(&mut outer, reverse);
+                for inner in 0..inner_len {
+                    if view[[inner, outer]] != Tile::Round { continue; }
+                    let mut i = outer;
+                    while i != min {
+                        increment(&mut i, !reverse);
+                        if view[[inner, i]] != Tile::Empty {
+                            increment(&mut i, reverse);
+                            break;
+                        }
+                    }
+                    if i == outer { continue; }
+                    view[[inner, i]] = Tile::Round;
+                    view[[inner, outer]] = Tile::Empty;
+                }
+            }
+        }
+
+        let (mirror, reverse) = match dir {
+            Dir::N => (false, false),
+            Dir::W => (true, false),
+            Dir::S => (false, true),
+            Dir::E => (true, true)
+        };
+        if mirror {
+            let (w, h) = self.0.dim();
+            let buffer = self.0.as_slice_memory_order_mut().unwrap();
+            let mut view = ArrayViewMut2::from_shape((h, w), buffer).unwrap();
+            tilt_view(view.view_mut(), reverse);
+        } else {
+            tilt_view(self.0.view_mut(), reverse);
         }
     }
     #[inline]
-    fn load_north(&self) -> usize {
+    fn cycle(&mut self) {
+        self.tilt(Dir::N);
+        self.tilt(Dir::W);
+        self.tilt(Dir::S);
+        self.tilt(Dir::E);
+    }
+    #[inline]
+    fn load(&self) -> usize {
         let h = self.0.len_of(Axis(1));
         self.0.axis_iter(Axis(1)).enumerate()
             .map( |(y, row)|
@@ -81,19 +146,35 @@ impl Platform {
             )
             .sum::<usize>()
     }
+    // FIXME: cycle will produce a cyclic load, not a steady state
+    #[inline]
+    fn steady_state_load(&mut self, infinity: usize) -> usize {
+        let mut last = self.load();
+        for _ in 0..infinity {
+            self.cycle();
+            let load = self.load();
+            if load == last { return load; }
+            last = load;
+        }
+        last
+    }
 }
 
 pub fn part1(input: &str) -> Answer {
     parse(input, Platform::parse)?
         .pipe( |mut platform| {
-            platform.tilt_north();
-            platform.load_north()
+            platform.tilt(Dir::N);
+            platform.load()
         } )
         .pipe( |result| Ok(Cow::Owned(result.to_string())) )
 }
 
 pub fn part2(input: &str) -> Answer {
-    todo!()
+    parse(input, Platform::parse)?
+        .pipe( |mut platform| {
+            platform.steady_state_load(1_00/*0_000_000*/)
+        } )
+        .pipe( |result| Ok(Cow::Owned(result.to_string())) )
 }
 
 inventory::submit! { Puzzle::new(2023, 14, 1, part1) }
@@ -119,9 +200,18 @@ mod test {
     const OUTPUT1: &str = "136";
 
     const INPUT2: &str = indoc! {"
-    
+        O....#....
+        O.OO#....#
+        .....##...
+        OO.#O....O
+        .O.....O#.
+        O.#..O.#.#
+        ..O..#O..O
+        .......O..
+        #....###..
+        #OO..#....
     "};
-    const OUTPUT2: &str = "";
+    const OUTPUT2: &str = "64";
 
     #[test]
     fn test1() {
