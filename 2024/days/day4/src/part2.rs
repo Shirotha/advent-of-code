3d93a34583b14dac5d59e8e0d95eecb501ccd329
Shirotha <shirotha+git@mailfence.com>
2024-12-12 15:03:15 +0100

    solved part2

--- 2024/days/day4/src/part2.rs
+++ 2024/days/day4/src/part2.rs
@@ -2,7 +2,38 @@ use advent_of_code::*;
 use day4::*;
 
 fn solve(input: Input) -> DResult<impl ToString> {
-    todo!("implement part 2 solution here")
+    fn invert(char: u8) -> Option<u8> {
+        match char {
+            b'M' => Some(b'S'),
+            b'S' => Some(b'M'),
+            _ => None,
+        }
+    }
+    let size = *input.data.size();
+    let check = |pos, dir| -> bool {
+        // SAFETY: assume: pos is not on the border
+        let corner = shift(pos, size, dir, 1).unwrap();
+        let Some(expected) = invert(input.data[corner]) else {
+            return false;
+        };
+        let other_corner = shift(pos, size, dir.invert(), 1).unwrap();
+        input.data[other_corner] == expected
+    };
+    let mut sum = 0;
+    for (pos, char) in &input.data {
+        if *char != b'A'
+            || pos[0] == 0
+            || pos[1] == 0
+            || pos[0] + 1 == size[0]
+            || pos[1] + 1 == size[1]
+        {
+            continue;
+        }
+        if check(pos, Dir::TopLeft) && check(pos, Dir::TopRight) {
+            sum += 1;
+        }
+    }
+    Ok(sum)
 }
 
 pub fn main() -> DResult<()> {
@@ -17,7 +48,7 @@ pub fn main() -> DResult<()> {
 mod test {
     use super::*;
 
-    const RESULT: &str = todo!("insert example output here");
+    const RESULT: &str = "9";
 
     #[test]
     fn test() -> DResult<()> {
