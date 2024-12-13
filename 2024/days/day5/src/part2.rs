6d11ce7a4d229896200de3a83a1e1cc8238ce6f6
Shirotha <shirotha+git@mailfence.com>
2024-12-13 11:58:16 +0100

    initial setup

---
+++ 2024/days/day5/src/part2.rs
@@ -0,0 +1,30 @@
+use advent_of_code::*;
+use day5::*;
+
+fn solve(input: Input) -> DResult<impl ToString> {
+    todo!("implement part 2 solution here")
+}
+
+pub fn main() -> DResult<()> {
+    let input = get_input()?;
+    let input = input.parse::<Input>()?;
+    let solution = solve(input)?;
+    println!("Solution: {}", solution.to_string());
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const RESULT: &str = todo!("insert example output here");
+
+    #[test]
+    fn test() -> DResult<()> {
+        let input = include_str!("../data/example.dat");
+        let input = input.parse::<Input>()?;
+        let result = solve(input)?;
+        assert_eq!(result.to_string(), RESULT);
+        Ok(())
+    }
+}
